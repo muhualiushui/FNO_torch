d1f0a64f0334cdf3b881699d7eeda021bd40081f
muhualiushui <shuzhen2@illinois.edu>
2025-05-03 19:46:14 -0700

    added algo

--- FNO_new.py
+++ FNO_new.py
@@ -106,7 +106,7 @@ class FNOnd(nn.Module):
         super().train()
         running = 0.0
         total = 0
-        pbar = tqdm(train_loader, desc='Train', leave=False, dynamic_ncols=False, position=0)
+        pbar = tqdm(train_loader, desc='Train', leave=False, dynamic_ncols=False, position=1)
         for batch in pbar:
             # Support both tuple/list batches and dict batches keyed by x_name/y_name
             if isinstance(batch, dict):
@@ -142,7 +142,7 @@ class FNOnd(nn.Module):
         val_running = 0.0
         total = 0
         with torch.no_grad():
-            pbar = tqdm(test_loader, desc='Valid', leave=False, dynamic_ncols=False, position=0)
+            pbar = tqdm(test_loader, desc='Valid', leave=False, dynamic_ncols=False, position=1)
             for batch in pbar:
                 # Support both tuple/list batches and dict batches keyed by x_name/y_name
                 if isinstance(batch, dict):
