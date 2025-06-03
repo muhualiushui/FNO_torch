e98b6461774eed5c8a25e0e0aaada997176022e3
muhualiushui <shuzhen2@illinois.edu>
2025-06-03 01:04:07 -0700

    clean

--- DiffSegtumor/code/utils/config.py
+++ DiffSegtumor/code/utils/config.py
@@ -4,11 +4,11 @@ class Config:
         if "brats_2d"in task:
             self.task = "brats_2d"
             self.save_dir = '../../Brats/data_prep/merged_nii'
-            self.modalities = ['flair', 't1', 't1ce', 't2', 'seg']  # Modalities to process
+            self.modalities = ['t2', 'seg']  # Modalities to process
 
             self.patch_size = (192, 192) 
             self.num_cls = 4
-            self.num_channels = 4
+            self.num_channels = 1
             self.n_filters = 32
             self.early_stop_patience = 50
             self.batch_size = 32                
@@ -16,11 +16,11 @@ class Config:
         elif "brats_diff"in task:
             self.task = "brats_diff"
             self.save_dir = '/media/ssd2/zhiwei/Brats/merged_nii/'
-            self.modalities = ['flair', 't1', 't1ce', 't2', 'seg']  # Modalities to process
+            self.modalities = ['t2', 'seg']  # Modalities to process
 
             self.patch_size = (192, 192) 
             self.num_cls = 4
-            self.num_channels = 4
+            self.num_channels = 1
             self.n_filters = 32
             self.early_stop_patience = 50
             self.batch_size = 32                
